66e0aa70da8f8b72a457ec36661b5fe0340b6cca
Dhruvedd <dhruv.daiya5@gmail.com>
2024-02-13 17:32:37 -0500

    hello2

--- GameOfLife/src/conwaygame/GameOfLife.java
+++ GameOfLife/src/conwaygame/GameOfLife.java
@@ -128,10 +128,133 @@ public class GameOfLife {
      * @param row row position of the cell
      * @return neighboringCells, the number of alive cells (at most 8).
      */
-    public int numOfAliveNeighbors (int row, int col) {
+    
+    private boolean CheckCell(int r, int c){      //new method to test all edgecases
+        
+        if((r < 0) && (c < grid[0].length) && ( 0 <= c)){
 
-        // WRITE YOUR CODE HERE
-        return 0; // update this line, provided so that code compiles
+            return getCellState(grid.length-1, c);
+        }
+
+        if((c < 0) && (r < grid.length) && ( 0 <= r)){
+
+            return getCellState(r, grid[0].length-1);
+        }
+
+        if((c >= grid[0].length) && (r < grid.length) && ( 0 <= r)){
+
+            return getCellState(r, 0);
+        }
+
+        if((r >= grid.length) && (c < grid[0].length) && ( 0 <= c)){
+
+            return getCellState(0, c);
+        }
+
+        if((r >= grid.length) && (c >= grid[0].length)){
+
+            return getCellState(0, 0);
+        }
+
+        if((r >= grid.length) && (c < 0)){
+
+            return getCellState(0, grid[0].length-1);
+        }
+
+        if((r < 0) && (c >= grid[0].length)){
+
+            return getCellState(grid.length-1, 0);
+        }
+
+        if((r < 0) && (c < 0)){
+
+            return getCellState(grid.length-1, grid[0].length-1);
+        }
+
+        
+        else return getCellState(r, c);
+
+
+    }
+
+    private int CheckIndex(int r, int c){      //new method to test all edgecases
+        
+        if((r < 0) && (c < grid[0].length) && ( 0 <= c)){
+
+            return 1;
+        }
+
+        if((c < 0) && (r < grid.length) && ( 0 <= r)){
+
+            return 2;
+        }
+
+        if((c >= grid[0].length) && (r < grid.length) && ( 0 <= r)){
+
+            return 2;
+        }
+
+        if((r >= grid.length) && (c < grid[0].length) && ( 0 <= c)){
+
+            return 1;
+        }
+
+        if((r >= grid.length) && (c >= grid[0].length)){
+
+            return 3;
+        }
+
+        if((r >= grid.length) && (c < 0)){
+
+            return 3;
+        }
+
+        if((r < 0) && (c >= grid[0].length)){
+
+            return 3;
+        }
+
+        if((r < 0) && (c < 0)){
+
+            return 3;
+        }
+
+        
+        else return 0;
+
+        //1 is when the row is wrong, 2 is when the column is wrong and 3 is when both are wrong
+
+
+    }
+    
+    
+    
+     public int numOfAliveNeighbors (int row, int col) {
+
+        
+        int r = row;
+        int c = col;
+
+        int count = 0;
+
+        if(CheckCell(r-1, c) == true){count++;}
+
+        if(CheckCell(r-1, c+1) == true){count++;}
+
+        if(CheckCell(r, c+1) == true){count++;}
+
+        if(CheckCell(r+1, c+1) == true){count++;}
+
+        if(CheckCell(r+1, c) == true){count++;}
+
+        if(CheckCell(r+1, c-1) == true){count++;}
+
+        if(CheckCell(r, c-1) == true){count++;}
+
+        if(CheckCell(r-1, c-1) == true){count++;}
+
+
+        return count; // update this line, provided so that code compiles
     }
 
     /**
@@ -143,7 +266,39 @@ public class GameOfLife {
     public boolean[][] computeNewGrid () {
 
         // WRITE YOUR CODE HERE
-        return new boolean[1][1];// update this line, provided so that code compiles
+
+        totalAliveCells = 0;
+
+        boolean[][] board = new boolean[grid.length][grid[0].length];
+
+        for(int i = 0; i < grid.length;i++){
+
+            for(int j = 0; j < grid[0].length;j++){
+
+                int a = numOfAliveNeighbors(i, j);
+
+                if(grid[i][j]){
+
+                    if(a >= 4){board[i][j] = false;}
+
+                    else if((a == 2) || (a == 3)){board[i][j] = true;}
+
+                    else if(a < 1){board[i][j] = false;}
+
+                    totalAliveCells++;
+                
+                
+                }
+
+                if((a == 3) && (grid[i][j] == false)){board[i][j] = true;}
+
+            }
+
+        }
+
+
+
+        return board;// update this line, provided so that code compiles
     }
 
     /**
@@ -154,7 +309,7 @@ public class GameOfLife {
      */
     public void nextGeneration () {
 
-        // WRITE YOUR CODE HERE
+        grid = computeNewGrid();
     }
 
     /**
@@ -163,7 +318,9 @@ public class GameOfLife {
      */
     public void nextGeneration (int n) {
 
-        // WRITE YOUR CODE HERE
+        for(int i = 0; i < n; i++){
+            nextGeneration();
+        }
     }
 
     /**
@@ -172,7 +329,144 @@ public class GameOfLife {
      */
     public int numOfCommunities() {
 
-        // WRITE YOUR CODE HERE
-        return 0; // update this line, provided so that code compiles
+        
+        WeightedQuickUnionUF gu = new WeightedQuickUnionUF(grid.length, grid[0].length);
+
+
+        
+        
+        for(int i = 0; i < grid.length;i++){
+
+            for(int j = 0; j < grid[0].length;j++){
+
+             if(getCellState(i, j)){
+
+                if(CheckCell(i-1, j) == true){
+
+                    if(CheckIndex(i-1, j) == 0){
+                        gu.union(i-1, j, i,j);
+                    }
+
+                    else if(CheckIndex(i-1, j) == 1){gu.union(grid.length-1, j, i,j);}
+                }
+
+                if(CheckCell(i-1, j+1) == true){
+
+                    if(CheckIndex(i-1, j+1) == 0){
+                        gu.union(i-1, j+1, i,j);
+                    }
+
+                    else if(CheckIndex(i-1, j+1) == 1){gu.union(grid.length-1, j, i,j);}
+
+                    else if(CheckIndex(i-1, j+1) == 2){gu.union(i-1, 0, i,j);}
+
+                    else if(CheckIndex(i-1, j+1) == 3){gu.union(grid.length-1, 0, i,j);}
+
+                   
+                }
+
+                if(CheckCell(i, j+1) == true){
+                    
+                    if(CheckIndex(i, j+1) == 0){
+                        gu.union(i, j+1, i,j);
+                    }
+
+                    else if(CheckIndex(i, j+1) == 2){gu.union(i, 0, i,j);}
+                    
+                }
+
+                if(CheckCell(i+1, j+1) == true){
+                   
+                    if(CheckIndex(i+1, j+1) == 0){
+                        gu.union(i+1, j+1, i,j);
+                    }
+
+                    else if(CheckIndex(i+1, j+1) == 1){gu.union(0, j+1, i,j);}
+
+                    else if(CheckIndex(i+1, j+1) == 2){gu.union(i+1, 0, i,j);}
+
+                    else if(CheckIndex(i+1, j+1) == 3){gu.union(0, 0, i,j);}
+                }
+
+                if(CheckCell(i+1, j) == true){
+                   
+                    if(CheckIndex(i+1, j) == 0){
+                        gu.union(i+1, j, i,j);
+                    }
+
+                    else if(CheckIndex(i+1, j) == 1){gu.union(0, j, i,j);}
+
+                }
+
+                if(CheckCell(i+1, j-1) == true){
+                    
+                    if(CheckIndex(i+1, j-1) == 0){
+                        gu.union(i+1, j-1, i,j);
+                    }
+
+                    else if(CheckIndex(i+1, j-1) == 1){gu.union(0, j-1, i,j);}
+
+                    else if(CheckIndex(i+1, j-1) == 2){gu.union(i+1, grid[0].length-1, i,j);}
+
+                    else if(CheckIndex(i+1, j-1) == 3){gu.union(0, grid[0].length-1, i,j);}
+
+                }
+
+                if(CheckCell(i, j-1) == true){
+                   
+                    if(CheckIndex(i, j-1) == 0){
+                        gu.union(i, j-1, i,j);
+                    }
+
+                    else if(CheckIndex(i, j-1) == 2){gu.union(i, grid[0].length-1, i,j);}
+                }
+
+                if(CheckCell(i-1, j-1) == true){
+                    if(CheckIndex(i-1, j-1) == 0){
+                        gu.union(i-1, j-1, i,j);
+                    }
+
+                    else if(CheckIndex(i-1, j-1) == 1){gu.union(grid.length-1, j-1, i,j);}
+
+                    else if(CheckIndex(i-1, j-1) == 2){gu.union(i-1, grid[0].length-1, i,j);}
+
+                    else if(CheckIndex(i-1, j-1) == 3){gu.union(grid.length-1, grid[0].length-1, i,j);}
+                }
+            }
+            
+            }
+
+        }
+
+        boolean[] hehe = new boolean[grid.length * grid[0].length];
+
+        for(int i = 0; i < grid.length;i++){
+
+            for(int j = 0; j < grid[0].length;j++){
+
+                
+                if(grid[i][j]){
+
+                    int root = gu.find(i, j);
+                    hehe[root] = true;
+                }
+ 
+
+            }
+
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < hehe.length; i++)
+        {
+            if(hehe[i]){
+                count++;
+            }
+        }
+
+
+        
+        return count; // update this line, provided so that code compiles
     }
 }
