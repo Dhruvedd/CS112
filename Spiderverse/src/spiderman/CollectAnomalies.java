912e6d8bb5c4955532bcb4e101dd52689ec17aa2
Dhruvedd <dhruv.daiya5@gmail.com>
2024-04-27 23:14:26 -0400

    4th method done

--- Spiderverse/src/spiderman/CollectAnomalies.java
+++ Spiderverse/src/spiderman/CollectAnomalies.java
@@ -48,6 +48,8 @@ public class CollectAnomalies {
     public int hub;
     public ArrayList<Anomaly> anomalies;
     public HashMap<Integer, ArrayList<Integer>> table;
+    public HashMap<Integer, ArrayList<Integer>> gra;
+    public ArrayList<Integer> dimList = new ArrayList<>();
     
     public static void main(String[] args) {
 
@@ -58,32 +60,171 @@ public class CollectAnomalies {
       //  }
 
         CollectAnomalies ca = new CollectAnomalies();
-        //String dims = args[0];
-        //ca.setTable(dims);
-        //String spidey = args[1];
-        //ca.defineHub(args[2]);
-        //ca.createVerse(spidey);
-        //ca.collectAno();
-        //ca.printAno(args[3]);
+        String dims = args[0];
+        ca.DimensionsCreate(dims);
+        ca.graph();
 
-
-
-        String dims = "dimension.in";
-        ca.setTable(dims);
-        String spidey = "spiderverse.in";
-        ca.defineHub("hub.in");
+        String spidey = args[1];
+        ca.defineHub(args[2]);
         ca.createVerse(spidey);
         ca.collectAno();
-        ca.printAno("collected.out");
+        ca.printAno(args[3]);
+
+
+
+        //String dims = "dimension.in";
+        //ca.DimensionsCreate(dims);
+        //ca.graph();
+//
+        //String spidey = "spiderverse.in";
+        //ca.defineHub("hub.in");
+        //ca.createVerse(spidey);
+        //ca.collectAno();
+        //ca.printAno("collected.out");
         
     }
 
-    public void setTable(String dims){
-        Clusters cl = new Clusters();
-        table = cl.getTable(dims);
+    public void DimensionsCreate(String In){
+ 
+ 
+        StdIn.setFile(In);
+        // StdIn.setFile(inFile);
+ 
+        int dimNum = StdIn.readInt();
+ 
+        int tableSize = StdIn.readInt();
+ 
+        double threshold = StdIn.readDouble();
+        StdIn.readLine();
+
+        table = new HashMap<>(tableSize);
+ 
+        double currDim = 0; 
+ 
+        for(int i = 0; i < dimNum; i++){
+
+            Dimension dime = new Dimension();
+
+            dime.setDimensionNum(StdIn.readInt());
+            dime.setCanonNum(StdIn.readInt());
+            dime.setWeight(StdIn.readInt());
+
+            if(!table.containsKey(dime.DimensionNum % tableSize)){
+                table.put(dime.getDimensionNum() % tableSize, new ArrayList<>());
+            }
+
+            table.get(dime.getDimensionNum() % tableSize).add(0, dime.getDimensionNum());
+            currDim++;
+
+            if((double)((currDim)/(table.size())) >= threshold ){
+                tableSize *= 2;
+                rehash(tableSize);
+            }
+        }
+        Connect();
     }
 
+    public void rehash(int newTableSize) {
+
+        HashMap<Integer, ArrayList<Integer>> tempTable = new HashMap<>(newTableSize);
+
+        for(ArrayList<Integer> list : table.values()){
+
+            for(int dimNum : list){
+
+                if(!tempTable.containsKey(dimNum % newTableSize)){
+                    tempTable.put(dimNum % newTableSize, new ArrayList<>());
+                }
+
+                tempTable.get(dimNum % newTableSize).add(0, dimNum);
+            }
+        }
+
+        table.clear();
+        table.putAll(tempTable);
+    }
+
+    public void Connect(){
+
+        for(int i=0 ; i<table.size() ; i++){
+
+            int conn1 = 0;
+            int conn2 = 0;
+
+            if(i==0){
+
+                conn1 = table.get(table.size()-1).get(0);
+                conn2 = table.get(table.size()-2).get(0);
+                
+                table.get(i).add(conn1);
+                table.get(i).add(conn2);
+            }
+
+            else if(i==1){
+
+                conn1 = table.get(i-1).get(0);
+                conn2 = table.get(table.size()-1).get(0);
+
+                table.get(i).add(conn1);
+                table.get(i).add(conn2);
+            }
+
+            else{
+
+                conn1 = table.get(i-1).get(0);
+                conn2 = table.get(i-2).get(0);
+
+                table.get(i).add(conn1);
+                table.get(i).add(conn2);
+            }
+        }
+    }
+
+    private boolean isThere(ArrayList<Integer> grap, int dime){  //helper method
+        return grap.contains(dime);
+    }
+
+    private int locateIndex(DimensionNode[] grap,int dim){
+        for(int i = 0; i < grap.length; i++){
+            if(grap[i].getData() == dim) return i;
+        }
+        return -1;
+    }
+
+    public void graph() {
     
+        // First pass to initialize the gra array
+        for (ArrayList<Integer> list : table.values()) {
+            for (int dim : list) {
+                if (!isThere(dimList, dim)) {
+                    dimList.add(dim);
+                }
+            }
+        }
+
+        int graphSize = dimList.size();
+
+        gra = new HashMap<>(graphSize);
+
+        for(int dim : dimList){
+
+                gra.put(dim, new ArrayList<>());
+                gra.get(dim).add(0,dim);
+        
+        }
+
+        for (ArrayList<Integer> list : table.values()) {
+
+            int headDim = list.get(0);
+
+            for (int dim : list) {
+
+                if(!gra.get(headDim).contains(dim)) gra.get(headDim).add(dim);
+                if(!gra.get(dim).contains(headDim)) gra.get(dim).add(headDim);
+
+            }
+        }
+    }
 
     public void defineHub(String file){
         StdIn.setFile(file);
@@ -152,11 +293,9 @@ public class CollectAnomalies {
         for(Anomaly ano : anomalies){
 
             ArrayList<Integer> toPath = new ArrayList<>();
-            ArrayList<Integer> fromPath = new ArrayList<>();
-
             ArrayList<Integer> fullPath = new ArrayList<>();
 
-            int tagetDim = ano.anomaly.currDim;
+            int targetDim = ano.anomaly.currDim;
 
             boolean isSpider = false;
 
@@ -165,55 +304,28 @@ public class CollectAnomalies {
                 ano.anomaly.Name = ano.anomaly.Name + " " + isSpidey(ano);
             }
 
-            boolean hubFound = false;
-            boolean targetFound = false;
+            toPath = shortestPath(gra, hub, targetDim);
 
-            toPath.add(hub);
-
-            for(ArrayList<Integer> list : table.values()){
-
-                if(hubFound){
-                    toPath.add(list.get(0));
-                }
-
-                for(int dim : list){
-
-                    if(dim == hub && !hubFound){
-                        hubFound = true;
-                        toPath.add(list.get(0));
-                    }
-                    if(dim == tagetDim){
-                        targetFound = true;
-                        break;
-                    }
-
-                }
-
-                if(targetFound) break;
-
-            }
-
-            ArrayList<Integer> temp = new ArrayList<>();
-            temp = toPath;
-
-            fromPath = temp;
-            Collections.reverse(fromPath);
-
-            if(!isSpider){
-                for(int dim : toPath){
-                    fullPath.add(dim);
-                }
-            }
-
-            fullPath.add(tagetDim);
-
-            for(int dim : fromPath){
+            for(int dim : toPath){
                 fullPath.add(dim);
             }
 
+            if(!isSpider){
+
+                Collections.reverse(toPath);
+                for(int dim :toPath){
+                    if(dim != targetDim){
+                        fullPath.add(dim);
+                    }
+                }
+
+            }
+
+            else{
+                Collections.reverse(fullPath);
+            }
             ano.path = fullPath;
         }
-
     }
 
     public void printAno(String file){
@@ -233,6 +345,56 @@ public class CollectAnomalies {
             StdOut.println();
         }
     }
+
+
+    public ArrayList<Integer> shortestPath(HashMap<Integer, ArrayList<Integer>> graph, int start, int end) {
+        // Visited map to keep track of visited nodes
+        HashMap<Integer, Boolean> visited = new HashMap<>();
+    
+        // Queue for BFS traversal
+        Queue<ArrayList<Integer>> queue = new LinkedList<>();
+    
+        // Parent map to keep track of the parent of each node in the shortest path
+        HashMap<Integer, Integer> parent = new HashMap<>();
+    
+        // Add start node to the queue and mark it as visited
+        ArrayList<Integer> path = new ArrayList<>();
+        path.add(start);
+        queue.add(path);
+        visited.put(start, true);
+    
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> currentPath = queue.poll();
+            int currentNode = currentPath.get(currentPath.size() - 1);
+    
+            // If we reach the end node, reconstruct and return the shortest path
+            if (currentNode == end) {
+                ArrayList<Integer> shortestPath = new ArrayList<>();
+                int node = end;
+                while (node != start) {
+                    shortestPath.add(0, node);
+                    node = parent.get(node);
+                }
+                shortestPath.add(0, start);
+                return shortestPath;
+            }
+    
+            // Explore neighbors of the current node
+            for (int neighbor : graph.get(currentNode)) {
+                if (!visited.containsKey(neighbor) || !visited.get(neighbor)) {
+                    visited.put(neighbor, true);
+                    ArrayList<Integer> newPath = new ArrayList<>(currentPath);
+                    newPath.add(neighbor);
+                    queue.add(newPath);
+                    parent.put(neighbor, currentNode); // Save parent of the neighbor
+                }
+            }
+        }
+    
+        // If no path exists between start and end nodes
+        return null;
+    }
+    
     
 
 
